a8640a9691d643ee84823de32ee0b545eb9dd2d2
Sean <sean.jennings1@gmail.com>
2024-02-18 13:42:51 -0700

    Add tmux_neww script to infect_dots.sh

--- tmux/tmux_nnew_copy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo hello!!!
-# pane=$(echo "$1" | tr -d '%')
-# pane_path=$(tmux show-environment TMUX_"$pane_id"_PATH | sed 's/^[^=]*=//g')
-# tmux set-environment NEWW "$pane_path"
-# tmux neww
